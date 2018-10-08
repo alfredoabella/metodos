4aa7e37231b39e6139bd0a59793da04755098900
alfredoabella <alfredoabelladorronsoro@gmail.com>
2018-10-08 09:50:13 +0200

    Inserción inicial en el repositorio del módulo metodos

---
+++ metodos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
